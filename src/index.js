19830f3aa169aec38db5fd50f65c54d55bbdc95c
Quin-Riley-A <quin.asselin1@gmail.com>
2022-10-22 19:58:07 -0400

    adjust form formatting and containing divs

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-function handleFormSubmission() {
-  
+function formListen() {
+  document.querySelector('form#request-form').addEventListener('submit', handleFormSubmission)
 }
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  console.log('here');
+}
+
+window.addEventListener('load', formListen);
+
